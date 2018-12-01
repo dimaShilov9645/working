3cf77f1a16e176987b1980b711d794407c56f4ba
Dmitriy <dimatu9645@gmal.com>
2018-12-01 12:20:21 +0300

    Сделал слайдер

--- repair/js/script.js
+++ repair/js/script.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
 	$('#button-callback').on('click', function() {
 		$('#modal-callback').toggleClass('modal_visible');
 	});
 	$('.modal__close').on('click', function() {
 		$('#modal-callback').toggleClass('modal_visible');
 	});
-});
+});*/
 
 /*document.querySelector('#button-callback').addEventListener('click', function() {
 	document.querySelector('#modal-callback').classList.toggle('modal_visible');
@@ -13,3 +13,40 @@ $(document).ready(function() {
 document.querySelector('.modal__close').addEventListener('click', function() {
 	document.querySelector('#modal-callback').classList.toggle('modal_visible');
 });*/
+jQuery(document).ready(function($) {
+    $('.popup-content').magnificPopup({
+        type: 'inline'
+    });
+});
+
+$(document).ready(function(){
+  $('.slider').slick({
+  	slidesToShow: 3,
+  	slidesToScroll: 1,
+  	nextArrow: '<div class="arrow arrow-right"></div>',
+  	prevArrow: '<div class="arrow arrow-left"></div>',
+  	responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
+});
