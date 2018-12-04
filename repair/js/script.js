28dc78136c49f0ba7e94dee407239410a6940ec1
Dmitriy <dimatu9645@gmal.com>
2018-12-04 09:39:07 +0300

    Отправка форм Ajax

--- repair/js/script.js
+++ repair/js/script.js
@@ -73,19 +73,19 @@ $(document).ready(function(){
     }	
   });
 
-
-
-$('.form-media').on('submit', function(){
-  var nameVal = $('[name = name]').val();
-      phoneVal = $('[name = phone]').val();
-  
-        $.ajax({
-          url: '/send.php',
-          data:{name:nameVal, phone:phoneVal},
-          success: function(){
-            alert('Load was performed.');
-          }
+$('form').submit(function(event) {
+      event.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function() {
+        $(this).find("input").val("");
+        alert("Сообщение успешно отправлено");
+        $("form").trigger("reset");
       });
-        return false;
+      return false;
     });
+
 });
+
